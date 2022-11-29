1f4eb7edd992bf004fad8dd52ce4b370ce8d4e24
colcha9088 <christopher.colcha9088@utc.edu.ec>
2022-11-29 07:37:21 -0500

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('header');
-		$this->load->view('welcome_message');
+		$this->load->view('usuarios/usuario');
 		$this->load->view('footer');
 	}
 }
